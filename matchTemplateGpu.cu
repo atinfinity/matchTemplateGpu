b49cd5f1cc696f3d7a7703f158ac2f57a666e7d5
atinfinity <dandelion1124@gmail.com>
2016-08-17 19:05:46 +0900

    fixed typo

--- matchTemplateGpu.cu
+++ matchTemplateGpu.cu
@@ -39,13 +39,13 @@ void launchMatchTemplateGpu
     cv::cudev::PtrStepSz<uchar> pDst =
         cv::cudev::PtrStepSz<uchar>(templ.rows, templ.cols * templ.channels(), templ.ptr<uchar>(), templ.step);
 
-    cv::cudev::PtrStepSz<float> pResut =
+    cv::cudev::PtrStepSz<float> pResult =
         cv::cudev::PtrStepSz<float>(result.rows, result.cols * result.channels(), result.ptr<float>(), result.step);
 
     const dim3 block(64, 2);
     const dim3 grid(cv::cudev::divUp(result.cols, block.x), cv::cudev::divUp(result.rows, block.y));
 
-    matchTemplateGpu<<<grid, block>>>(pImg, pDst, pResut);
+    matchTemplateGpu<<<grid, block>>>(pImg, pDst, pResult);
 
     CV_CUDEV_SAFE_CALL(cudaGetLastError());
     CV_CUDEV_SAFE_CALL(cudaDeviceSynchronize());
